fc3c23662897716cf4009d4ed7f7fc486ff333fd
rucka <miroslav.rucka@posam.sk>
2016-02-16 18:39:23 +0100

    rest api - db connection

--- script/startSensors.sh
+++ script/startSensors.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/github/sensors/
 forever stop sensors.js
-forever start -l /home/suslik/log/sensors/out -p /home/suslik -a -d sensors.js
+forever start -l /home/suslik/log/sensors/out -p /home/suslik -a -d sensors.js prod
