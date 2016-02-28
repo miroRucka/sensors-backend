8fe0170f093668898bd06fee33b4bafddfc49a9c
rucka <miroslav.rucka@posam.sk>
2016-02-28 20:43:34 +0100

    rest api - db connection

--- config/express.js
+++ config/express.js
@@ -3,10 +3,7 @@
  */
 'use strict';
 var logger = require('./logging');
-var bodyParser = require('body-parser');
 module.exports = function (app) {
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(bodyParser.json());
     app.use(function (err, req, res, next) {
         logger.error(err);
         res.send(500, 'Something broke!');
